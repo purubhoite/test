28a6162cf26fb29260900251c52e80b886887f69
Puru Bhoite <psbedits26@gmail.com>
2026-02-05 02:16:28 -0800

    Convert Java class to C++ for addition program

---
+++ calc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int add(int a,int b){
+  return a+b;
+}
+
+int main() {
+int a,b;
+cout<<"Addition Program"<<endl
+cout<<"Enter first no - ";
+cin>>a;
+cout<<"Enter second no - ";
+cin>>b;
+cout<<"Result - "<<add(a,b);
+return 0;
+}
