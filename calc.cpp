1760102c205ab8dc5c680e34ae07f54ed2b57f75
purubhoite <psbedits26@gmail.com>
2026-02-05 10:40:32 +0000

    Addition to Subtraction

--- calc.cpp
+++ calc.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int add(int a,int b){
-  return a+b;
+int sub(int a,int b){
+  return a-b;
 }
 
 int main() {
 int a,b;
-cout<<"Addition Program"<<endl;
+cout<<"Subtraction Program"<<endl;
 cout<<"Enter first no - ";
 cin>>a;
 cout<<"Enter second no - ";
 cin>>b;
-cout<<"Result - "<<add(a,b);
+cout<<"Result - "<<sub(a,b);
 return 0;
 }
